cdc19ac174e7fd4406b8646c181763bf3bd9af77
James Collins <james.collins@outlook.com.au>
2023-07-05 14:13:27 +1000

    added loadChunkRadius

--- src/main/java/com/stemcraft/utility/Util.java
+++ src/main/java/com/stemcraft/utility/Util.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import com.stemcraft.STEMCraft;
 
 public class Util {
     public static String capitalize(String string, boolean allWords) {
@@ -107,4 +109,44 @@ public class Util {
         return null;
     }
 
+    public static boolean loadChunkRadius(Location pos, int radius, boolean force) {
+        int baseX = (int)(pos.getX() / 16.0D);
+        int baseZ = (int)(pos.getZ() / 16.0D);
+        boolean success = true;
+
+        for(int x = -1 * radius; x <= radius; x++) {
+            for(int z = -1 * radius; z <= radius; z++) {
+                int chunkX = baseX + x;
+                int chunkZ = baseZ + z;
+                World world = pos.getWorld();
+
+                try {
+                    world.addPluginChunkTicket(chunkX, chunkZ, STEMCraft.getInstance());
+                }
+                catch (NoSuchMethodError e) {
+                    /* empty */
+                }
+
+                Chunk chunk = world.getChunkAt(chunkX, chunkZ);
+                if(!world.isChunkLoaded(chunk)) {
+                    boolean load = false;
+
+                    try {
+                        load = world.loadChunk(chunkX, chunkZ, true);
+                    } catch (RuntimeException e) {
+                        load = false;
+                    }
+
+                    if(!load) {
+                        if(success) {
+                            success = false;
+                        }
+                        System.out.println("Failed to load and activate Chunk at X: " + (chunkX * 16) + " Z: " + (chunkZ * 16) + " in " + world.getName());
+                    }
+                }
+            }
+        }
+
+        return success;
+    }
 }
