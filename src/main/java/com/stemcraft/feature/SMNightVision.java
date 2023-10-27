4ec7f0e8947c7f3f886bac7b12fdad9bd95bbfc3
James Collins <james.collins@outlook.com.au>
2023-10-27 11:06:59 +1000

    restore previous effect and extend command params

--- src/main/java/com/stemcraft/feature/SMNightVision.java
+++ src/main/java/com/stemcraft/feature/SMNightVision.java
@@ -1,49 +1,131 @@
 package com.stemcraft.feature;
 
+import java.util.UUID;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import com.stemcraft.core.SMFeature;
 import com.stemcraft.core.SMMessenger;
+import com.stemcraft.core.SMPersistent;
 import com.stemcraft.core.command.SMCommand;
 
+/**
+ * Allows players to enable/disable night vision via command.
+ */
 public class SMNightVision extends SMFeature {
+
     /**
-     * When the feature is enabled
+     * Enables the night vision feature by registering the command.
+     *
+     * @return true if the feature is successfully enabled, otherwise false.
      */
     @Override
     protected Boolean onEnable() {
         new SMCommand("nightvision")
             .alias("nv")
-            .permission("stemcraft.nightvision")
+            .permission("stemcraft.command.nightvision")
+            .tabComplete("enable", "{player}")
+            .tabComplete("disable", "{player}")
+            .tabComplete("toggle", "{player}")
             .action(ctx -> {
-                // if (!(sender instanceof Player)) {
-                //     this.plugin.getLanguageManager().sendPhrase(sender, "CMD_ONLY_PLAYERS");
-                //     return true;
-                // }
+                Player targetPlayer = ctx.getArgAsPlayer(2, ctx.player);
+                String action = "toggle";
 
-                toggleNightVision((Player)ctx.sender);
+                // get action (if exists)
+                if(ctx.args.length > 0) {
+                    action = ctx.args[0].toLowerCase();
+                }
+
+                // Check player exists when issued from console
+                ctx.checkBooleanLocale(!(ctx.fromConsole() && ctx.args.length == 2), "CMD_PLAYER_REQ_FROM_CONSOLE");
+
+                // Check target player exists
+                ctx.checkNotNullLocale(targetPlayer, "CMD_PLAYER_NOT_FOUND");
+
+                if(action.equals("toggle")) {
+                    toggleNightVision(targetPlayer);
+                } else if(action.equals("enable")) {
+                    enableNightVision(targetPlayer);
+                } else if(action.equals("disable")) {
+                    disableNightVision(targetPlayer);
+                } else {
+                    ctx.returnErrorLocale("NIGHTVISION_USAGE");
+                }
             })
             .register();
 
         return true;
     }
 
-    private void enableNightVision(Player player) {
+    /**
+     * Enables night vision effect for the specified player. If the player already has
+     * a night vision effect, it's saved before applying the new effect.
+     *
+     * @param player The player for whom the night vision effect is to be enabled.
+     */
+    public static void enableNightVision(Player player) {
+        UUID playerUUID = player.getUniqueId();
+
+        // Save any current nightvision
+        if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION) && !SMPersistent.exists(SMNightVision.class, playerUUID.toString())) {
+            for (PotionEffect effect : player.getActivePotionEffects()) {
+                if (effect.getType() == PotionEffectType.NIGHT_VISION) {
+                    SMPersistent.set(SMNightVision.class, playerUUID.toString(), effect);
+                    break;
+                }
+            }
+        }
+
         player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0, false, false));
         SMMessenger.infoLocale(player, "NIGHTVISION_ENABLED");
     }
 
-    private void disableNightVision(Player player) {
+    /**
+     * Disables night vision effect for the specified player. If the player had a previously
+     * saved night vision effect, it's restored after removing the current effect.
+     *
+     * @param player The player for whom the night vision effect is to be disabled.
+     */
+    public static void disableNightVision(Player player) {
+        UUID playerUUID = player.getUniqueId();
+
         player.removePotionEffect(PotionEffectType.NIGHT_VISION);
+
+        // Restore any previous nightvision
+        if(SMPersistent.exists(SMNightVision.class, playerUUID.toString())) {
+            player.addPotionEffect(SMPersistent.getObject(SMNightVision.class, playerUUID.toString(), PotionEffect.class));
+            SMPersistent.clear(SMNightVision.class, playerUUID.toString());
+        }
+
         SMMessenger.infoLocale(player, "NIGHTVISION_DISABLED");
     }
 
-    private void toggleNightVision(Player player) {
+    /**
+     * Toggles the night vision effect for the specified player. If the player currently has
+     * the effect, it's disabled, otherwise, it's enabled.
+     *
+     * @param player The player for whom the night vision effect is to be toggled.
+     */
+    public static void toggleNightVision(Player player) {
         if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
             disableNightVision(player);
         } else {
             enableNightVision(player);
+        }
+    }
+
+    /**
+     * Clears the night vision effect for the specified player without restoring any 
+     * previously saved effects.
+     *
+     * @param player The player for whom the night vision effect is to be cleared.
+     */
+    public static void clearNightVision(Player player) {
+        UUID playerUUID = player.getUniqueId();
+
+        if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
+            SMPersistent.clear(SMNightVision.class, playerUUID.toString());
+            player.removePotionEffect(PotionEffectType.NIGHT_VISION);
         }
     }
 }
