704ab337627c49724dc2443401ac69c8d02bc66a
James Collins <james.collins@outlook.com.au>
2023-10-29 20:55:46 +1000

    added getEnchantment support

--- src/main/java/com/stemcraft/core/SMBridge.java
+++ src/main/java/com/stemcraft/core/SMBridge.java
@@ -3,8 +3,10 @@ package com.stemcraft.core;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Server;
 import org.bukkit.command.CommandMap;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import com.stemcraft.STEMCraft;
@@ -14,9 +16,7 @@ import lombok.NonNull;
 /**
  * STEMCraft cross-version compatibility class.
  * 
- * Look up for many methods enabling the plugin
- * to be compatible with later versions of the
- * server.
+ * Look up for many methods enabling the plugin to be compatible with later versions of the server.
  */
 public final class SMBridge {
     /**
@@ -32,49 +32,50 @@ public final class SMBridge {
             bukkitCommandMap.setAccessible(true);
             CommandMap commandMap = (CommandMap) bukkitCommandMap.get(server);
             return commandMap;
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return null;
     }
 
-     //     public static CommandMap getCommandMap() {
-//         final Class<?> craftServer = getOBCClass("CraftServer");
-        
-// try {
-// 			return (SimpleCommandMap) craftServer.getDeclaredMethod("getCommandMap").invoke(Bukkit.getServer());
+    // public static CommandMap getCommandMap() {
+    // final Class<?> craftServer = getOBCClass("CraftServer");
 
-// 		} catch (final ReflectiveOperationException ex) {
+    // try {
+    // return (SimpleCommandMap) craftServer.getDeclaredMethod("getCommandMap").invoke(Bukkit.getServer());
 
-// 			try {
-// 				return ReflectionUtil.getFieldContent(Bukkit.getServer(), "commandMap");
+    // } catch (final ReflectiveOperationException ex) {
 
-// 			} catch (final Throwable ex2) {
-// 				throw new FoException(ex2, "Unable to get the command map");
-// 			}
-// 		}
+    // try {
+    // return ReflectionUtil.getFieldContent(Bukkit.getServer(), "commandMap");
 
-//         try {
-//             final Field bukkitCommandMap = this.plugin.getServer().getClass().getDeclaredField("commandMap");
+    // } catch (final Throwable ex2) {
+    // throw new FoException(ex2, "Unable to get the command map");
+    // }
+    // }
 
-//             bukkitCommandMap.setAccessible(true);
-//             CommandMap commandMap = (CommandMap) bukkitCommandMap.get(this.plugin.getServer());
-//             return commandMap;
-//         } catch(Exception e) {
-//             e.printStackTrace();
-//         }
+    // try {
+    // final Field bukkitCommandMap = this.plugin.getServer().getClass().getDeclaredField("commandMap");
 
-//         return null;
+    // bukkitCommandMap.setAccessible(true);
+    // CommandMap commandMap = (CommandMap) bukkitCommandMap.get(this.plugin.getServer());
+    // return commandMap;
+    // } catch(Exception e) {
+    // e.printStackTrace();
+    // }
 
-//     }
+    // return null;
+
+    // }
 
     public static String getName(final Entity entity) {
         try {
             return entity.getName();
 
         } catch (final NoSuchMethodError t) {
-            return entity instanceof Player ? ((Player) entity).getName() : SMCommon.beautifyCapitalize(entity.getType());
+            return entity instanceof Player ? ((Player) entity).getName()
+                : SMCommon.beautifyCapitalize(entity.getType());
         }
     }
 
@@ -151,27 +152,45 @@ public final class SMBridge {
     }
 
     public static Method getMethod(final Class<?> clazz, final String methodName, final Class<?>... args) {
-		try {
-			final Method method = clazz.getMethod(methodName, args);
-			method.setAccessible(true);
-			return method;
-		} catch (final NoSuchMethodException e) {
+        try {
+            final Method method = clazz.getMethod(methodName, args);
+            method.setAccessible(true);
+            return method;
+        } catch (final NoSuchMethodException e) {
             /* empty */
-		}
+        }
 
-		return null;
-	}
+        return null;
+    }
 
     @SuppressWarnings("unchecked")
     public static <T> T invokeStatic(@NonNull final Method method, final Object... params) {
-		try {
-			SMValid.checkBoolean(Modifier.isStatic(method.getModifiers()),
-					"Method " + method.getName() + " must be static to be invoked through invokeStatic with params: " + SMCommon.join(params));
+        try {
+            SMValid.checkBoolean(Modifier.isStatic(method.getModifiers()),
+                "Method " + method.getName() + " must be static to be invoked through invokeStatic with params: "
+                    + SMCommon.join(params));
 
-			return (T) method.invoke(null, params);
+            return (T) method.invoke(null, params);
 
-		} catch (final ReflectiveOperationException ex) {
-			throw new SMBridgeException(ex, "Could not invoke static method " + method + " with params " + SMCommon.join(params, ", "));
-		}
-	}
+        } catch (final ReflectiveOperationException ex) {
+            throw new SMBridgeException(ex,
+                "Could not invoke static method " + method + " with params " + SMCommon.join(params, ", "));
+        }
+    }
+
+    /**
+     * Get an Enchantment object by name (string)
+     * 
+     * @param name The string name of the enchantment.
+     * @return An Enchantment object or null if doesn't exist.
+     */
+    @SuppressWarnings("deprecation")
+    public static Enchantment getEnchantment(String name) {
+        if (SMMinecraftVersion.atLeast(SMMinecraftVersion.V.v1_13)) {
+            NamespacedKey key = NamespacedKey.minecraft(name.toLowerCase());
+            return Enchantment.getByKey(key);
+        }
+
+        return Enchantment.getByName(name.toUpperCase());
+    }
 }
