c3615f6a3b10062520fae9ac6c099570930880a6
James Collins <james.collins@outlook.com.au>
2023-11-03 08:36:20 +1000

    formatting

--- src/main/java/com/stemcraft/core/SMLocale.java
+++ src/main/java/com/stemcraft/core/SMLocale.java
@@ -48,7 +48,7 @@ public class SMLocale {
                 String fileName = file.getName();
                 if (fileName.startsWith(prefix) && fileName.endsWith(extension)) {
                     String locale = fileName.substring(prefix.length(), fileName.length() - extension.length());
-                    if(!localeFiles.containsKey(locale)) {
+                    if (!localeFiles.containsKey(locale)) {
                         STEMCraft.info("Replaced locale: " + locale);
                         localeFiles.put(locale, new SMConfigFile(fileName));
                     }
@@ -59,6 +59,7 @@ public class SMLocale {
 
     /**
      * Get a locale string from the messages file.
+     * 
      * @param id
      * @return
      */
@@ -68,6 +69,7 @@ public class SMLocale {
 
     /**
      * Get a locale string from the messages file using the players locale.
+     * 
      * @param player
      * @param id
      * @return
@@ -78,15 +80,16 @@ public class SMLocale {
 
     /**
      * Get a locale string from the messages file using the sender locale.
+     * 
      * @param sender
      * @param id
      * @return
      */
     public final static String get(CommandSender sender, String id) {
         String locale = DEFAULT_LOCALE;
-        
-        if(sender instanceof Player) {
-            locale = ((Player)sender).getLocale();
+
+        if (sender instanceof Player) {
+            locale = ((Player) sender).getLocale();
         }
 
         return get(locale, id);
@@ -94,6 +97,7 @@ public class SMLocale {
 
     /**
      * Get a locale string from the messages file using the specified locale.
+     * 
      * @param locale
      * @param id
      * @return
@@ -103,48 +107,27 @@ public class SMLocale {
     }
 
     /**
-     * Get a locale string from the messages file using the specified locale.
-     * If it does not exist, use the defValue.
+     * Get a locale string from the messages file using the specified locale. If it does not exist, use the defValue.
+     * 
      * @param locale
      * @param id
      * @param defValue
      * @return
      */
     public final static String get(String locale, String id, String defValue) {
-        if(localeFiles.containsKey(locale)) {
+        if (localeFiles.containsKey(locale)) {
             SMConfigFile localeFile = localeFiles.get(locale);
-            if(localeFile.contains(id)) {
+            if (localeFile.contains(id)) {
                 return localeFiles.get(locale).getString(id);
             }
         }
 
         SMDebugger.debug("locale", "Missing locale string " + locale + " for " + id);
 
-        if(locale != "en") {
+        if (locale != "en") {
             return get("en", id, defValue);
         }
 
         return defValue;
-    }
-
-    /**
-     * Save a locale string.
-     * @param locale
-     * @param id
-     * @param string
-     */
-    public final static void setDefault(String locale, String id, String string) {
-        SMConfigFile localeFile = null;
-
-        if(!localeFiles.containsKey(locale)) {
-            localeFile = new SMConfigFile(prefix + locale + extension);
-            localeFiles.put(locale, localeFile);
-        } else {
-            localeFile = localeFiles.get(locale);
-        }
-
-        if(!localeFile.contains(id)) {
-            localeFile.setDefault(id, string);
-        }
     }
 }
