98bffc934a2ca5deb3283a2e575b6705eacde2c7
James Collins <james.collins@outlook.com.au>
2023-11-03 15:26:05 +1000

    tab completion contains instead of startsWith

--- src/main/java/com/stemcraft/core/command/SMCommand.java
+++ src/main/java/com/stemcraft/core/command/SMCommand.java
@@ -53,6 +53,7 @@ public class SMCommand implements TabCompleter {
 
     /**
      * Constructor
+     * 
      * @param label
      */
     public SMCommand(String label) {
@@ -61,6 +62,7 @@ public class SMCommand implements TabCompleter {
 
     /**
      * Add aliases to the command.
+     * 
      * @param aliases
      * @return
      */
@@ -71,6 +73,7 @@ public class SMCommand implements TabCompleter {
 
     /**
      * Set the permission required for the command.
+     * 
      * @param permission
      * @return
      */
@@ -98,6 +101,7 @@ public class SMCommand implements TabCompleter {
 
     /**
      * Set the callback processor when the command is used.
+     * 
      * @param processor
      * @return
      */
@@ -108,11 +112,12 @@ public class SMCommand implements TabCompleter {
 
     /**
      * Register the command on the server
+     * 
      * @return
      */
     public SMCommand register() {
         CommandMap commandMap = SMBridge.getCommandMap();
-        if(commandMap != null) {
+        if (commandMap != null) {
             PluginCommand pluginCommand = null;
 
             try {
@@ -120,11 +125,11 @@ public class SMCommand implements TabCompleter {
                 c.setAccessible(true);
 
                 pluginCommand = c.newInstance(label, STEMCraft.getPlugin());
-            } catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-        
-            if(pluginCommand != null) {
+
+            if (pluginCommand != null) {
                 pluginCommand.setTabCompleter(this);
 
                 if (this.aliases.size() > 0) {
@@ -133,22 +138,22 @@ public class SMCommand implements TabCompleter {
 
                 pluginCommand.setExecutor((sender, command, label, args) -> {
                     SMCommandContext context = new SMCommandContext(this, sender, label, args);
-                    
+
                     try {
-                        if(permission != "" && !sender.hasPermission(permission)) {
+                        if (permission != "" && !sender.hasPermission(permission)) {
                             throw new SMCommandException("You do not have permission to use this command");
                         }
 
                         processor.process(context);
-                    } catch(final SMCommandException ex) {
-                        if(ex.getMessages() != null) {
+                    } catch (final SMCommandException ex) {
+                        if (ex.getMessages() != null) {
                             SMMessenger.error(sender, Arrays.asList(ex.getMessages()));
                         }
                     }
 
                     return true;
-                });    
-                
+                });
+
                 if (aliases.size() > 0) {
                     pluginCommand.setAliases(aliases);
                 }
@@ -166,50 +171,50 @@ public class SMCommand implements TabCompleter {
         Map<String, List<String>> supplierList = new HashMap<>();
 
         tabCompletionList.forEach(list -> {
-            if(list.length > args.length - 1) {
+            if (list.length > args.length - 1) {
                 Boolean ignoreList = false;
 
                 for (int i = 0; i < args.length - 1; i++) {
                     String[] listToCheck = null;
 
-                    if(list[i] instanceof String) {
+                    if (list[i] instanceof String) {
                         String listItem = list[i].toString();
 
-                        if(listItem.startsWith("{") && listItem.endsWith("}")) {
+                        if (listItem.startsWith("{") && listItem.endsWith("}")) {
                             String tabCompleteFunc = listItem.substring(1, listItem.length() - 1);
                             List<String> tabCompleteFuncList = SMTabComplete.get(tabCompleteFunc);
 
                             supplierList.put(tabCompleteFunc, tabCompleteFuncList);
-                            if(!tabCompleteFuncList.contains(args[i])) {
+                            if (!tabCompleteFuncList.contains(args[i])) {
                                 ignoreList = true;
                                 break;
                             }
 
                         } else {
                             // string does not match arg
-                            if(!args[i].equalsIgnoreCase(listItem)) {
+                            if (!args[i].equalsIgnoreCase(listItem)) {
                                 ignoreList = true;
                                 break;
                             }
                         }
-                    } else if(list[i] instanceof String[]) {
-                        listToCheck = (String[])list[i];
+                    } else if (list[i] instanceof String[]) {
+                        listToCheck = (String[]) list[i];
                     }
 
-                    if(listToCheck != null) {
-                        if(!Arrays.asList(listToCheck).contains(args[i])) {
+                    if (listToCheck != null) {
+                        if (!Arrays.asList(listToCheck).contains(args[i])) {
                             ignoreList = true;
                             break;
                         }
                     }
                 }
 
-                if(!ignoreList) {
-                    if(list[args.length - 1] instanceof String) {
-                        String listItem = (String)list[args.length - 1];
-                        if(listItem.startsWith("{") && listItem.endsWith("}")) {
+                if (!ignoreList) {
+                    if (list[args.length - 1] instanceof String) {
+                        String listItem = (String) list[args.length - 1];
+                        if (listItem.startsWith("{") && listItem.endsWith("}")) {
                             String tabCompleteFunc = listItem.substring(1, listItem.length() - 1);
-                            if(!supplierList.containsKey(tabCompleteFunc)) {
+                            if (!supplierList.containsKey(tabCompleteFunc)) {
                                 List<String> tabCompleteFuncList = SMTabComplete.get(tabCompleteFunc);
                                 supplierList.put(tabCompleteFunc, tabCompleteFuncList);
                             }
@@ -218,21 +223,24 @@ public class SMCommand implements TabCompleter {
                         } else {
                             stringList.add(listItem);
                         }
-                    } else if(list[args.length - 1] instanceof String[]) { 
-                        stringList.addAll(Arrays.asList((String[])list[args.length - 1]));
+                    } else if (list[args.length - 1] instanceof String[]) {
+                        stringList.addAll(Arrays.asList((String[]) list[args.length - 1]));
                     }
                 }
             }
         });
 
         // remove non-matching items
-        if(args[args.length - 1].length() > 0) {
+        if (args[args.length - 1].length() > 0) {
             String startingWith = args[args.length - 1];
             Iterator<String> iterator = stringList.iterator();
 
             while (iterator.hasNext()) {
                 String item = iterator.next();
-                if (!item.startsWith(startingWith)) {
+                // if (!item.startsWith(startingWith)) {
+                // iterator.remove();
+                // }
+                if (!item.contains(startingWith)) {
                     iterator.remove();
                 }
             }
@@ -245,102 +253,103 @@ public class SMCommand implements TabCompleter {
         // System.arraycopy(args, 0, argsList, 1, args.length);
 
         // if (!(sender instanceof Player) || args.length == 0) {
-        //     return null;
+        // return null;
         // }
 
         // // Check if the current args match the elements in tabCompletion
         // for (String[] completion : this.tabCompletionList) {
-        //     if(completion.length < argsList.length) {
-        //         continue;
-        //     }
+        // if(completion.length < argsList.length) {
+        // continue;
+        // }
 
-        //     boolean match = true;
-        //     for (int i = 0; i < argsList.length - 1; i++) {
-        //         if(completion[i].startsWith("%") && completion[i].endsWith("%")) {
-        //             String key = completion[i].substring(1, completion[i].length() - 1);
-                    
-        //             if (this.tabCompletionPlaceholders.containsKey(key)) {
-        //                 BiFunction<Server, String, List<String>> biFunction = tabCompletionPlaceholders.get(key);
+        // boolean match = true;
+        // for (int i = 0; i < argsList.length - 1; i++) {
+        // if(completion[i].startsWith("%") && completion[i].endsWith("%")) {
+        // String key = completion[i].substring(1, completion[i].length() - 1);
 
-        //                 if(biFunction.apply(sender.getServer(), argsList[i]).isEmpty()) {
-        //                     match = false;
-        //                     break;
-        //                 }
-        //             }
-        //         } else if (!completion[i].equals(argsList[i])) {
-        //             match = false;
-        //             break;
-        //         }
-        //     }
+        // if (this.tabCompletionPlaceholders.containsKey(key)) {
+        // BiFunction<Server, String, List<String>> biFunction = tabCompletionPlaceholders.get(key);
 
-        //     if (match) {
-        //         String targetCompletion = completion[argsList.length - 1];
+        // if(biFunction.apply(sender.getServer(), argsList[i]).isEmpty()) {
+        // match = false;
+        // break;
+        // }
+        // }
+        // } else if (!completion[i].equals(argsList[i])) {
+        // match = false;
+        // break;
+        // }
+        // }
 
-        //         if(targetCompletion.startsWith("%") && targetCompletion.endsWith("%")) {
-        //             String key = targetCompletion.substring(1, targetCompletion.length() - 1);
-                    
-        //             if (this.tabCompletionPlaceholders.containsKey(key)) {
-        //                 BiFunction<Server, String, List<String>> biFunction = tabCompletionPlaceholders.get(key);
+        // if (match) {
+        // String targetCompletion = completion[argsList.length - 1];
 
-        //                 for(String valueItem : biFunction.apply(sender.getServer(), argsList[argsList.length - 1])) {
-        //                     tabCompleteList.add(valueItem);
-        //                 }
-        //             }
-        //         } else if(targetCompletion.startsWith(argsList[argsList.length - 1])) {
-        //             tabCompleteList.add(targetCompletion);
-        //         }
-        //     }
+        // if(targetCompletion.startsWith("%") && targetCompletion.endsWith("%")) {
+        // String key = targetCompletion.substring(1, targetCompletion.length() - 1);
+
+        // if (this.tabCompletionPlaceholders.containsKey(key)) {
+        // BiFunction<Server, String, List<String>> biFunction = tabCompletionPlaceholders.get(key);
+
+        // for(String valueItem : biFunction.apply(sender.getServer(), argsList[argsList.length - 1])) {
+        // tabCompleteList.add(valueItem);
+        // }
+        // }
+        // } else if(targetCompletion.startsWith(argsList[argsList.length - 1])) {
+        // tabCompleteList.add(targetCompletion);
+        // }
+        // }
         // }
 
         // return tabCompleteList;
     }
 
     // AFTER
-    
-    
+
+
     // protected HashMap<String, BiFunction<Server, String, List<String>>> tabCompletionPlaceholders = new HashMap<>();
     // protected HashMap<String, BiFunction<CommandSender, String[], Boolean>> stemcraftOptions = new HashMap<>();
 
     // public void addTabCompletions(String[][] tabCompletions) {
-    //     if(tabCompletions != null && tabCompletions.length > 0) {
-    //         int newSize = this.tabCompletionList.length + tabCompletions.length;
-    //         String[][] combinedArray = new String[newSize][];
+    // if(tabCompletions != null && tabCompletions.length > 0) {
+    // int newSize = this.tabCompletionList.length + tabCompletions.length;
+    // String[][] combinedArray = new String[newSize][];
 
-    //         System.arraycopy(this.tabCompletionList, 0, combinedArray, 0, this.tabCompletionList.length);
-    //         System.arraycopy(tabCompletions, 0, combinedArray, this.tabCompletionList.length, tabCompletions.length);
-    //         this.tabCompletionList = combinedArray;
-    //     }
+    // System.arraycopy(this.tabCompletionList, 0, combinedArray, 0, this.tabCompletionList.length);
+    // System.arraycopy(tabCompletions, 0, combinedArray, this.tabCompletionList.length, tabCompletions.length);
+    // this.tabCompletionList = combinedArray;
+    // }
     // }
 
 
 
     // public void registerTabPlaceholder(String placeholder, BiFunction<Server, String, List<String>> callback) {
-    //     this.tabCompletionPlaceholders.put(placeholder, callback);
+    // this.tabCompletionPlaceholders.put(placeholder, callback);
     // }
 
     // public void registerStemCraftOption(String option, BiFunction<CommandSender, String[], Boolean> callback) {
-    //     this.registerStemCraftOption(option, callback, null);
+    // this.registerStemCraftOption(option, callback, null);
     // }
-    
-    // public void registerStemCraftOption(String option, BiFunction<CommandSender, String[], Boolean> callback, String[][] tabCompletions) {
-    //     this.stemcraftOptions.put(option, callback);
-    //     if(tabCompletions != null) {
-    //         this.addTabCompletions(tabCompletions);
-    //     } else {
-    //         this.addTabCompletions(new String[][] {{"stemcraft", option}});
-    //     }
+
+    // public void registerStemCraftOption(String option, BiFunction<CommandSender, String[], Boolean> callback,
+    // String[][] tabCompletions) {
+    // this.stemcraftOptions.put(option, callback);
+    // if(tabCompletions != null) {
+    // this.addTabCompletions(tabCompletions);
+    // } else {
+    // this.addTabCompletions(new String[][] {{"stemcraft", option}});
+    // }
     // }
 
     // protected List<String> player(Server server, String match) {
-    //     List<String> playerList = new ArrayList<>();
+    // List<String> playerList = new ArrayList<>();
 
-    //     for(Player player : server.getOnlinePlayers()) {
-    //         String playerName = player.getName();
-    //         if(playerName.startsWith(match)) {
-    //             playerList.add(playerName);
-    //         }
-    //     }
+    // for(Player player : server.getOnlinePlayers()) {
+    // String playerName = player.getName();
+    // if(playerName.startsWith(match)) {
+    // playerList.add(playerName);
+    // }
+    // }
 
-    //     return playerList;
+    // return playerList;
     // }
 }
