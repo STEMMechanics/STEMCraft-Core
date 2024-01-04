1d9578aabeca7a1910d4e2db1306d3e51eed54fd
James Collins <james.collins@outlook.com.au>
2024-01-04 12:37:27 +1000

    added geyser support

---
+++ src/main/java/com/stemcraft/feature/SMGeyser.java
@@ -0,0 +1,83 @@
+package com.stemcraft.feature;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+import org.geysermc.geyser.api.GeyserApi;
+import org.geysermc.geyser.api.connection.GeyserConnection;
+import com.stemcraft.STEMCraft;
+import com.stemcraft.core.SMBridge;
+import com.stemcraft.core.SMDependency;
+import com.stemcraft.core.SMFeature;
+import com.stemcraft.core.event.SMEvent;
+import dev.lone.itemsadder.api.CustomStack;
+import dev.lone.itemsadder.api.FontImages.FontImageWrapper;
+
+/**
+ * Geyser plugin support
+ */
+public class SMGeyser extends SMFeature {
+    private static String dependantName = "GeyserMC";
+    private static GeyserApi api = null;
+
+    /**
+     * Called when the feature is requested to be loaded.
+     * 
+     * @return If the feature loaded successfully.
+     */
+    @Override
+    public Boolean onLoad() {
+        if (!super.onLoad()) {
+            return false;
+        }
+
+        if (Bukkit.getPluginManager().getPlugin(dependantName) == null) {
+            STEMCraft.warning(dependantName + " is not loaded. Features requiring this plugin won't be available");
+        }
+
+        return true;
+    }
+
+    /**
+     * Called when the feature is requested to be enabled.
+     * 
+     * @return If the feature enabled successfully.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    protected Boolean onEnable() {
+        return true;
+    }
+
+    /**
+     * Returns if the Geyser plugin is loaded and ready.
+     * 
+     * @return If the plugin is ready.
+     */
+    public static Boolean isGeyserReady() {
+        if (api != null) {
+            return true;
+        }
+
+        api = GeyserApi.api();
+        return api != null;
+    }
+
+    /**
+     * Test if a player is a BedRock player
+     * 
+     * @param player The player to test.
+     * @return If the player is a geyser (or null if the dependency failed)
+     */
+    public static Boolean isBedrockPlayer(Player player) {
+        if (!isGeyserReady()) {
+            return null;
+        }
+
+        return api.isBedrockPlayer(player.getUniqueId());
+    }
+}
