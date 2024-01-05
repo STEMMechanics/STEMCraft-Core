84cbc890f5e364662272b45c36503ad794e930e0
James Collins <james.collins@outlook.com.au>
2024-01-06 07:36:26 +1000

    added getArgInt and Float Failures

--- src/main/java/com/stemcraft/core/command/SMCommandContext.java
+++ src/main/java/com/stemcraft/core/command/SMCommandContext.java
@@ -223,12 +223,32 @@ public class SMCommandContext {
      * @param idx
      * @return
      */
-    public int getArgInt(int idx, int defValue) {
+    private int getArgInt(int idx, int defValue, String onFail, Boolean isLocale) {
         try {
             return idx > 0 && this.args.size() >= idx ? Integer.parseInt(this.args.get(idx - 1)) : defValue;
         } catch (Exception e) {
+            if (onFail != null) {
+                if (isLocale) {
+                    throwCommandException(SMLocale.get(sender, onFail));
+                } else {
+                    throwCommandException(onFail);
+                }
+            }
+
             return defValue;
         }
+    }
+
+    public int getArgInt(int idx, int defValue) {
+        return getArgInt(idx, defValue, null, false);
+    }
+
+    public int getArgInt(int idx, String onFail) {
+        return getArgInt(idx, 0, onFail, false);
+    }
+
+    public int getArgIntLocale(int idx, String onFail) {
+        return getArgInt(idx, 0, onFail, true);
     }
 
     /**
@@ -237,12 +257,32 @@ public class SMCommandContext {
      * @param idx
      * @return
      */
-    public float getArgFloat(int idx, float defValue) {
+    private float getArgFloat(int idx, float defValue, String onFail, Boolean isLocale) {
         try {
             return idx > 0 && this.args.size() >= idx ? Float.parseFloat(this.args.get(idx - 1)) : defValue;
         } catch (Exception e) {
+            if (onFail != null) {
+                if (isLocale) {
+                    throwCommandException(SMLocale.get(sender, onFail));
+                } else {
+                    throwCommandException(onFail);
+                }
+            }
+
             return defValue;
         }
+    }
+
+    public float getArgFloat(int idx, float defValue) {
+        return getArgFloat(idx, defValue, null, false);
+    }
+
+    public float getArgFloat(int idx, String onFail) {
+        return getArgFloat(idx, 0f, onFail, false);
+    }
+
+    public float getArgFloatLocale(int idx, String onFail) {
+        return getArgFloat(idx, 0f, onFail, true);
     }
 
     /**
