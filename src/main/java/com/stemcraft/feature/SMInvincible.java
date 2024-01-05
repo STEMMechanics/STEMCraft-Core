48e595868936adafd31fb741a9c66b6850fa11fc
James Collins <james.collins@outlook.com.au>
2024-01-05 17:40:24 +1000

    added

---
+++ src/main/java/com/stemcraft/feature/SMInvincible.java
@@ -0,0 +1,45 @@
+package com.stemcraft.feature;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import com.stemcraft.core.SMFeature;
+import com.stemcraft.core.event.SMEvent;
+
+/**
+ * Allows players to enable/disable night vision via command.
+ */
+public class SMInvincible extends SMFeature {
+
+    /**
+     * Enables the night vision feature by registering the command.
+     *
+     * @return true if the feature is successfully enabled, otherwise false.
+     */
+    @Override
+    protected Boolean onEnable() {
+        SMEvent.register(PlayerJoinEvent.class, ctx -> {
+            setInvincible(ctx.event.getPlayer());
+        });
+
+        SMEvent.register(PlayerGameModeChangeEvent.class, ctx -> {
+            setInvincible(ctx.event.getPlayer());
+        });
+
+        SMEvent.register(PlayerTeleportEvent.class, ctx -> {
+            if (!ctx.event.getCause().equals(PlayerTeleportEvent.TeleportCause.END_PORTAL)
+                && !ctx.event.getCause().equals(PlayerTeleportEvent.TeleportCause.NETHER_PORTAL)) {
+                setInvincible(ctx.event.getPlayer());
+            }
+        });
+
+        return true;
+    }
+
+    public void setInvincible(Player player) {
+        player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 400, 255));
+    }
+}
